70a2dc66045efdcbe5e890a6f4a317909a8dbe9a
Cutiesamba01 <rhodagbemi@gmail.com>
2024-09-12 22:53:00 +0100

    corrections made for 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,9 +4,12 @@ import hidden_4
 import sys
 
 if __name__ == "__main__":
+    # Get all names defined in the module
     names = dir(hidden_4)
-    filter_names = [name for name in names if not name.stsratswith("__")]
+    # Filter out names that starts with __
+    filter_names = [name for name in names if not name.startswith("__")]
+    # Sorts names alphabetically
     filter_names.sort()
-
+    # Prints each name on a new line
     for name in filter_names:
         print(name)
--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -4,8 +4,6 @@ from variable_load_5 import a
 
 
 def main():
-
-
     print("{}".format(a))
 
 
